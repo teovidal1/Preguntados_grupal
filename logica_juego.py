2260ea3910e4e1dbcb11211af2ff019073d17381
Teo Vidal <vidalteo23@gmail.com>
2024-11-11 08:47:11 -0300

    insta

--- logica_juego.py
+++ logica_juego.py
@@ -73,13 +73,14 @@ def jugar(lista_usuarios:list):
     if len(set(lista_usuarios[indice_jugador]["coronas"]))==4:
         lista_usuarios[indice_jugador]["victorias"] += 1 
         print (f"¡¡VICTORIA!!, AHORA TENES {lista_usuarios[indice_jugador]["victorias"]} victoria/s")
-        lista_usuarios[indice_jugador]["vidas"] = 3
-
+        
     if lista_usuarios[indice_jugador]["vidas"]==0:
         print (f"Perdiste!")
-        lista_usuarios[indice_jugador]["vidas"] = 3
+        
 
+    lista_usuarios[indice_jugador]["vidas"] = 3
     lista_usuarios[indice_jugador]["partidas_jugadas"] +=1
+    lista_usuarios[indice_jugador]["coronas"]=()
 
     guardar_usuarios(lista_usuarios)
 
